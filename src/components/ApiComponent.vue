1bfcd6dbdf1d0210f102b8d9f497018d52baa25d
Pablo GM <cdomonge@gmail.com>
2023-02-16 16:26:18 -0600

    AjusteEntrega

--- src/components/ApiComponent.vue
+++ src/components/ApiComponent.vue
@@ -1,15 +1,3 @@
-<template>
-    <div>
-<p>Hola</p>
-    </div>
-</template>
-
-<script>
-    export default {
-        name:"ApiComponent"
-    }
-</script>
 
 <style lang="scss" scoped>
-
-</style>
+   
